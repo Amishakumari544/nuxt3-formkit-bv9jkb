e7405662fef0bc44009d29f10260e5a93b558bb9
Amishakumari544 <amishakumari544@gmail.com>
2023-08-21 11:48:03 +0530

    Initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="m-4">
+    <FormKit
+      type="text"
+      label="Username"
+      :validation="[
+        ['required'],
+        ['length', 3, 20],
+        ['matches', /^[A-Za-z0-9\_]+$/],
+      ]"
+      validation-visibility="live"
+      :validation-messages="{
+        matches: 'Please enter a valid Username.',
+      }"
+      placeholder="Enter your Username..."
+    />
+  </div>
+</template>
+
+<style></style>
